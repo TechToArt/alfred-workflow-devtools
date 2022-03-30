699848946b4589b20c143a4f6817468d4c477a0e
artist <1822661289@qq.com>
2022-03-30 22:44:26 +0800

    action功能处理跳链中&连接符

--- src/action.py
+++ src/action.py
@@ -13,12 +13,12 @@ def main(wf):
 
     # 因为直接传递json字符串会被截断，输入参数为经过编码后的json字符串
     device_info = json.loads(utils.decode_str(args[0]))
-    clipboard_str = utils.read_clipboard_str()
+    clipboard_str = utils.read_clipboard_str().replace("&", "\\&")
 
     notify_title = "alfred打开app"
     # 读取到apk路径则进行安装
     if "://" in clipboard_str:
-        command = utils.get_adb_path() + " -s %s shell am start %s" % (device_info["device_id"], clipboard_str)
+        command = utils.get_adb_path() + " -s %s shell am start \"%s\"" % (device_info["device_id"], clipboard_str)
         result = utils.exec_cmd(command)
         notify(notify_title.decode("utf-8"), result)
     else:
